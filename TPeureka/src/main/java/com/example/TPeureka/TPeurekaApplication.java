830f99ecb770414b2e4ef66a8b83dac11450ae10
moussa201 <38951287+moussa201@users.noreply.github.com>
2023-01-30 12:04:45 +0100

    projet spring boot
    creation du projet

---
+++ TPeureka/src/main/java/com/example/TPeureka/TPeurekaApplication.java
@@ -0,0 +1,13 @@
+package com.example.TPeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TPeurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TPeurekaApplication.class, args);
+	}
+
+}
---
+++ TPeureka/src/test/java/com/example/TPeureka/TPeurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TPeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TPeurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
